a567a8e5ba27e9a7c55b7956347bd71d612ca62e
Angel Ivan Castell Rovira <al004140@gmail.com>
2026-01-03 22:32:10 +0100

    update helper scripts

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,3 +1,17 @@
+#! /bin/bash
+
 curl -X POST http://localhost:8080/pelicula \
   -H "Content-Type: application/json" \
-  -d '{"name":"Terminator","year":1995,"director":"Arnold s"}'
+  -d '{"name":"Terminator","year":1984,"director":"James Cameron"}'
+
+curl -X POST http://localhost:8080/pelicula \
+  -H "Content-Type: application/json" \
+  -d '{"name":"El nombre de la rosa","year":1986,"director":"Jean-Jacques Annaud"}'
+
+curl -X POST http://localhost:8080/pelicula \
+  -H "Content-Type: application/json" \
+  -d '{"name":"Gladiator","year":2000,"director":"Ridley Scott"}'
+
+curl -X POST http://localhost:8080/pelicula \
+  -H "Content-Type: application/json" \
+  -d '{"name":"Idiocracia","year":2006,"director":"Mike Judge"}'
